0ac610c2cac5abcb0bb2dc660ea5a5e81485d76f
milaptojanice <janicechen31997@hotmail.com>
2024-01-06 15:49:48 -0500

    janice commit

--- js/swipper.js
+++ js/swipper.js
@@ -10,3 +10,13 @@
             prevEl: '.swiper-button-prev',
         },
     });
+
+
+    $(document).ready(function() {
+        $('.has-animation').each(function(index) {
+          $(this).delay($(this).data('delay')).queue(function(){
+            $(this).addClass('animate-in');
+          });
+        });
+      });
+      
